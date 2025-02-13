337f3c37a5519ce7d1af1adbc020e6a1f64eca3d
OluwapelumiElisha <elishaoluwapelumi38@gmail.com>
2025-02-13 14:47:14 +0100

    Correction on Apostrophe

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -3,8 +3,6 @@ import { useState } from "react";
 import Image from "next/image";
 import BurgerMenuIcon from "@/assets/Burger menu icon.png";
 import closeMenuIcon from "@/assets/Vector.png";
-// import Logo from "../../public/images/Dependly.png";
-import Logo from '../../public/images/Dependly.png'
 import { Button } from "./ui/button";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
@@ -46,7 +44,7 @@ const Navbar = () => {
         {/* Logo */}
         <Link href={"/"}>
           <Image
-            src={Logo}
+            src='/images/Dependly.png'
             alt="Dependly Logo"
             className="lg:w-[180px] lg:h-[54px] md:w-[180px] md:h-[54px] sm:w-[180px] sm:h-[54px] w-[124px] h-[38px]"
             width={100}
